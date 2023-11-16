93a1be7a1c90efb678ed6858670d36bf73d45674
HanzierToo <hendikailukman38@gmail.com>
2023-11-16 13:18:48 +0700

    feat(UI): Revamped color scheme (CSS)

--- src/main/java/ui_app/CalcApplication.java
+++ src/main/java/ui_app/CalcApplication.java
@@ -3,16 +3,19 @@ package ui_app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class CalcApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        stage.getIcons().add(new Image(Objects.requireNonNull(CalcApplication.class.getResourceAsStream("icon.png"))));
         FXMLLoader fxmlLoader = new FXMLLoader(CalcApplication.class.getResource("calc-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 420, 420);
-        scene.getStylesheets().add(CalcApplication.class.getResource("global.css").toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(CalcApplication.class.getResource("global.css")).toExternalForm());
         stage.setTitle("Commsult Calculator Demo");
         stage.setScene(scene);
         stage.show();
