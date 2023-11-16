b9d33da6acf145d5f045cdfb20675c4373801a08
HanzierToo <hendikailukman38@gmail.com>
2023-11-16 12:55:21 +0700

    feat(UI): Calculate on Enter formulaInput

--- src/main/java/ui_app/CalcController.java
+++ src/main/java/ui_app/CalcController.java
@@ -1,9 +1,12 @@
 package ui_app;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.util.Duration;
 
@@ -41,6 +44,14 @@ public class CalcController {
         resultTooltip5.setHideDelay(Duration.millis(100));
         mainGrid.setVgap(10);
         keypadGrid.setHgap(30);
+        formulaInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode().equals(KeyCode.ENTER)) {
+                    onCalculateButtonClick();
+                }
+            }
+        });
     }
 
     @FXML
